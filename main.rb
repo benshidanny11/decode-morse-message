e428362d07fe31cb9ecf05b6e7c1bf8c21c49351
benshidanny11 <benshidanny11@gmail.com>
2022-09-21 14:34:38 +0200

    Resolve bug

--- main.rb
+++ main.rb
@@ -20,12 +20,13 @@ def decode_char(char)
     end
     actual_word
   end
-#   ------
+
 def decode_sentence(coded_sentence)
     msg = []
-    coded_sentence.split('   ').each do |word|
+    coded_sentence.split(' ').each do |word|
       msg.push(decode_word(word))
     end
     msg.join(' ')
   end
+
   puts decode_sentence('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
